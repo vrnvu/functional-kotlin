021a7a56688efd8e7ae5cc131c45cf3f6714dd54
Arnau Díaz <diazarnau0@gmail.com>
2019-06-13 22:42:48 +0200

    Partial Application

---
+++ src/basic/PartialApplication.kt
@@ -0,0 +1,18 @@
+package basic
+
+/**
+ * Partially applying a function means creating a new function by pre-filling some of the arguments to the original function.
+ * In Kotlin we need a helper function
+ */
+fun main() {
+    val add: (Int) -> ((Int) -> Int) = { x -> { y -> x + y } }
+    val increment = add(1)
+    val incrementTwo = increment(2)
+    val incrementFive = increment(5)
+
+    println(add)
+    println(incrementTwo)
+    println(incrementFive)
+
+}
+
