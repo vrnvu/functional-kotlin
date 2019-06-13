fa9a15bed46048e5f48a610c5115aea9715ac7a1
Arnau Díaz <diazarnau0@gmail.com>
2019-06-13 22:13:13 +0200

    Higher Order Function

--- src/basic/HigherOrderFunction.kt
+++ src/basic/HigherOrderFunction.kt
@@ -4,9 +4,14 @@ package basic
  * A function which takes a function as an argument and/or returns a function.
  */
 fun main() {
-    // Function
-    val sum: (Int, Int) -> Int = { a, b -> a + b }
+    // Function 1
+    val double: (Int) -> Int = { a -> a * 2 }
+
+    // Function 2
+    val triple: (Int) -> Int = { a -> a * 3 }
+
     // Higher Order Function
-    val operator = {}
-    println(sum(2, 3))
+    val addOne: ((Int) -> Int, Int) -> Int = {f, a -> f(a) + 1}
+    println(addOne(double, 2))
+    println(addOne(triple, 2))
 }
